2079246cbae631279c301ae8b9478d0971191cc6
Laurent Peuch <cortex@worlddomination.be>
2012-04-11 22:09:30 +0200

    configure the project to install basic.blog

--- urlab/urls.py
+++ urlab/urls.py
@@ -15,4 +15,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^news/', include('basic.blog.urls')),
+    url(r'^comments/', include('django.contrib.comments.urls')),
 )
